89130111931ec42b978fa9df730771fefa1c65d5
Ayumu Kojima <youki.6230@gmail.com>
2022-05-17 23:44:44 +0900

    Tweetモデルを定義

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweet(models.Model):
+    name = models.CharField(max_length=20)
+    text = models.CharField(max_length=100)
+    image = models.TextField()
+    pub_date = models.DateTimeField('date publiched')
