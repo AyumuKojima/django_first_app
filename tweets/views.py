ecb431ec1b3944a0bd8e63581b0bbed1a96bc14a
Ayumu Kojima <youki.6230@gmail.com>
2022-05-18 21:29:41 +0900

    下準備

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
 # Create your views here.
+from .models import Tweet
 
 def index(request):
-    return HttpResponse("Hello!")
+    tweets = Tweet.objects.order_by('-pub_date')
+    return render(request, 'tweets/index.html', { 'tweets': tweets })
+
+def detail(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id)
+    return render(request, 'tweets/detail.html', { 'tweet': tweet })
