17eab1621dde700722f38e08851d49c32d0fa53c
Ayumu Kojima <youki.6230@gmail.com>
2022-05-18 14:59:53 +0900

    Update models.py

--- tweets/models.py
+++ tweets/models.py
@@ -7,3 +7,6 @@ class Tweet(models.Model):
     text = models.CharField(max_length=100)
     image = models.TextField()
     pub_date = models.DateTimeField('date publiched')
+
+    def __str__(self):
+        return self.text
