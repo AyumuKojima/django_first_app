5f89cb649c5445dddb2e04781ba7541f5090b928
Ayumu Kojima <youki.6230@gmail.com>
2022-05-19 19:40:47 +0900

    適切な情報を入力すれば投稿できるようにする

--- tweets/urls.py
+++ tweets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('post', views.post, name='post'),
+    path('create', views.create, name='create'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views import generic
+from django.utils import timezone
+from django.urls import reverse
 # Create your views here.
 from .models import Tweet
 
@@ -16,3 +19,9 @@ class DetailView(generic.DetailView):
 
 def post(request):
     return render(request, 'tweets/post.html')
+
+def create(request):
+    data = request.POST
+    posted_tweet = Tweet(name=data['name'], image=data['image'], text=data['text'], pub_date=timezone.now())
+    posted_tweet.save()
+    return HttpResponseRedirect(reverse('tweets:index'))
