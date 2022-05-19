527f6d19e1d28528504da9f787feedfbd7ba3999
Ayumu Kojima <youki.6230@gmail.com>
2022-05-19 17:06:47 +0900

    ヘッダーロゴにurlを設定

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tweets'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:tweet_id>/', views.detail, name='detail'),
