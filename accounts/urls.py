4edf2b5bba74b34625bc5dae26864aa0e15cdb13
Ayumu Kojima <youki.6230@gmail.com>
2022-05-21 17:19:08 +0900

    ログアウト機能を実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.sign_in, name='sign_in'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('sign_out/', views.sign_out, name='sign_out'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
+import imp
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout, login, authenticate
+from django.urls import reverse
 # Create your views here.
 
 from django.contrib.auth.models import User
 
 def sign_in(request):
-    return HttpRequest('ログインします。')
+    return HttpResponse('ログインします。')
+
+def sign_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('tweets:index'))
